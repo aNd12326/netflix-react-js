e86a2012078c54aee105235699172b539daaf1ac
Anderson Ventosilla <andersonventosilla0@gmail.com>
2022-05-07 19:54:18 -0500

    11:41

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className=''>
         <h1 className='text-red-600 text-4xl font-bold cursor-pointer '>NETFLIX</h1>
         <div>
-            <button>Sign In</button>
+            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign In</button>
             <button>Sign Up</button>
         </div>
     </div>
